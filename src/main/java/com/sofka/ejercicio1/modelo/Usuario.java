cb2a97c4da141f57feca1f9e0f9624a71cccc346
CTironi <tironi741@gmail.com>
2023-01-02 19:58:43 -0300

    Creamos la estructura del proyecto y la entidad usuario

---
+++ src/main/java/com/sofka/ejercicio1/modelo/Usuario.java
@@ -0,0 +1,28 @@
+package com.sofka.ejercicio1.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nombre;
+    @Column(nullable = false)
+    private String apellido;
+    @Column(length = 20)
+    private String telefono;
+    @Column(nullable = false)
+    private String direccion;
+
+}
