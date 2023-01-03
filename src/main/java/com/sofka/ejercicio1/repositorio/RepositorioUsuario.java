d6dc177f7c517e5b611a66b21c694c6f5011a409
CTironi <tironi741@gmail.com>
2023-01-02 21:27:37 -0300

    Creamos la clase ControladorUsuario

--- src/main/java/com/sofka/ejercicio1/repositorio/RepositorioUsuario.java
+++ src/main/java/com/sofka/ejercicio1/repositorio/RepositorioUsuario.java
@@ -2,6 +2,8 @@ package com.sofka.ejercicio1.repositorio;
 
 import com.sofka.ejercicio1.modelo.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositorioUsuario extends JpaRepository<Usuario, Long> {
 }
